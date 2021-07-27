29fdcf1333307e5f464b60af385bea32eb7b4a9b
AlinaPristinskaya <pristinskayaalina9@gmail.com>
2021-07-28 01:54:13 +0300

    28.07

--- src/App.js
+++ src/App.js
@@ -1,57 +1,59 @@
-import React,{Component} from 'react';
-import PropTypes from 'prop-types'
-import FeedbackOptions from './components/Feedback/'
+import {useState} from 'react';
+import FeedbackOptions from './components/Feedback'
 import Statistics from './components/Statistics'
 import SectionTitle from './components/SectionTitle'
 
-class App extends Component {
-  static defaultProps={
-      good:0,
-      neutral:0,
-      bad:0
-  }
-  static propTypes={
-    goods:PropTypes.number,
-    neutral:PropTypes.number,
-    bad:PropTypes.number
-  }
-  state = {
-    good: 0,
-    neutral: 7,
-    bad: 2
-  }
-  onLeaveFeedback=e=>{
-   const buttonClick= e.target.innerText;
-   this.setState(prevState=>({[buttonClick]:prevState[buttonClick]+1}))
+function App(){
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad,setBad]=useState(0);
+  const option=['good','neutral','bad'];
+  
+
+  const onLeaveFeedback = event => {
+    const buttonClick=event.target.innerText
+    console.log(event)
+
+    switch (buttonClick) {
+      case 'good':
+        setGood(prev=>prev+1);
+        break;
+
+      case 'neutral':
+        setNeutral(prev=>prev+1);
+        break;
+
+      case 'bad':
+        setBad(prev=>prev+1);
+        break;  
+
+      default:
+        return;
+    }}
    
-  }
-  
-  countTotalFeedback(){
-    return this.state.good+this.state.neutral+this.state.bad
-  }
-  countPositiveFeedbackPercentage(){
-    const positivePercentage=this.state.good*100/this.countTotalFeedback()
-    return Math.round(positivePercentage)
-  }
-  
-  render() {
-    const {good,neutral,bad}=this.state;
-    
-       return (
-            <>
-      <SectionTitle>
-      <h1>Please leave feedback</h1>        
-      <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onLeaveFeedback}/>              
-      <h3>Statistics</h3>
-      </SectionTitle>        
-      <Statistics good={good} bad={bad} neutral={neutral} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()} />
-            </>
-    );
-
-  }}
-
-    
-  
-
+  const countTotalFeedback=()=>{
+      return good+neutral+bad
+    }
+  const countPositiveFeedbackPercentage=()=>{
+      const positivePercentage=good*100/countTotalFeedback()
+      return Math.round(positivePercentage)
+    }
+    return (
+      <>
+         <SectionTitle>
+         <h1>Please leave feedback</h1>        
+         <FeedbackOptions options={option} onLeaveFeedback={onLeaveFeedback}/>              
+         <h3>Statistics</h3>
+         </SectionTitle>        
+         <Statistics good={good} bad={bad} neutral={neutral} total={countTotalFeedback()} positivePercentage={countPositiveFeedbackPercentage()} />
+      </>);   
+}
 
 export default App;
+
+
+    
+  
+
+
+
--- src/components/statistics/statistics.js
+++ src/components/statistics/statistics.js
@@ -1,38 +1,20 @@
-import React,{Component} from 'react';
-import PropTypes from 'prop-types';
+
 import SectionTitle from '../SectionTitle'
 import Notification from '../Notification';
 
-
-
-class Statistics extends Component{
-    static propTypes={
-    total:PropTypes.number,
-    positivePercentage:PropTypes.number
+const Statistics=({good,neutral,bad,total,positivePercentage})=>{
+  if (total===0) {
+    return <SectionTitle><Notification messadge={`No feedback given`}/></SectionTitle>;
   }
-      
-   
-  render=()=>{
-      const {good,bad,neutral,total,positivePercentage}=this.props
-      if (total===0) {
-        return <SectionTitle><Notification messadge={`No feedback given`}/></SectionTitle>;
-      }
-          return (
-            
-         
-            <div>
-          <SectionTitle><p>Кол-во хороших комментариев: {good}</p></SectionTitle>
-          <SectionTitle><p>Кол-во нейтральных комментариев: {neutral}</p></SectionTitle>
-          <SectionTitle><p>Кол-во плохих комментариев: {bad}</p></SectionTitle>
-          <SectionTitle><p>Общее кол-во комментариев:{total}</p></SectionTitle>
-          <SectionTitle><p>% хороших комментариев от общего кол-ва:{positivePercentage}%</p></SectionTitle>
-         
-        </div>
-
-        )
-    }
+   return (
+     <div>
+      <SectionTitle><p>Кол-во хороших комментариев: {good}</p></SectionTitle>
+      <SectionTitle><p>Кол-во нейтральных комментариев: {neutral}</p></SectionTitle>
+      <SectionTitle><p>Кол-во плохих комментариев: {bad}</p></SectionTitle>
+      <SectionTitle><p>Общее кол-во комментариев:{total}</p></SectionTitle>
+      <SectionTitle><p>% хороших комментариев от общего кол-ва:{positivePercentage}%</p></SectionTitle>
+    </div>
+    )
 }
-
-
 
 export default Statistics;
